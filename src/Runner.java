17204a2e7e32731d149907626a5f9d4c986578da
denisoetken <denisoetken@hotmail.de>
2016-09-30 10:15:14 +0200

    Threads machen kaputt

--- src/Runner.java
+++ src/Runner.java
@@ -7,20 +7,29 @@ public class Runner {
 
         Rechner r = new Rechner(3, 5);
 
-        new Thread() {
-            public void run() {
-                r.setA(5);
-                r.setB(8);
-                r.rechne();
-            }
-        }.start();
+        while (true) {
+            new Thread() {
+                public void run() {
+                    r.setA(5);
+                    r.setB(8);
+                    if (r.rechne() != 13) {
+                        System.out.println("1. Thread Falsch");
+                        System.exit(0);
+                    }
+                }
+            }.start();
 
-        new Thread() {
-            public void run() {
-                r.setA(2);
-                r.setB(7);
-                r.rechne();
-            }
-        }.start();
+            new Thread() {
+                public void run() {
+                    r.setA(2);
+                    r.setB(7);
+                    r.rechne();
+                    if (r.rechne() != 9) {
+                        System.out.println("2. Thread Falsch");
+                        System.exit(0);
+                    }
+                }
+            }.start();
+        }
     }
 }
